a8b012d10579ae9184edc7db317ff00c0a58b504
somrat <somrat945hossain@gmail.com>
2024-01-17 02:38:42 +0600

    login page and firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:assesment/ui/screen/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+
   runApp(const MyApp());
 }
 
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+// // ...
+
+// await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -11,13 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
     );
   }
 }
-
--- lib/ui/screen/auth/login_screen.dart
+++ lib/ui/screen/auth/login_screen.dart
@@ -1,15 +1,88 @@
 import 'package:flutter/material.dart';
 
-class _LoginScreenState extends StatefulWidget {
-  const _LoginScreenState({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<_LoginScreenState> createState() => __LoginScreenStateState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class __LoginScreenStateState extends State<_LoginScreenState> {
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              const Text("Welcome Back!",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text("Sign in to account",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.grey,
+                  )),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    )),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    )),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20.0),
+              InkWell(
+                onTap: () {
+                  // print("tapp");
+                },
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue.shade300,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text("Log In"),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
