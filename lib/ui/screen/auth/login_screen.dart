1a0e550279ecd27b8e94aaffaf1cb4c34b58e634
somrat <somrat945hossain@gmail.com>
2024-01-17 00:49:17 +0600

    initial commit

---
+++ lib/ui/screen/auth/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _LoginScreenState extends StatefulWidget {
+  const _LoginScreenState({super.key});
+
+  @override
+  State<_LoginScreenState> createState() => __LoginScreenStateState();
+}
+
+class __LoginScreenStateState extends State<_LoginScreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
